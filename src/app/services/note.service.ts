d5c490cde1a0c1d5cd2ced60e96f201f364cb6a5
Krasilnikov Roman <krasilnikov_ri@transset.ru>
2018-11-15 17:32:19 +0300

    DatePipe for execution-date @ NoteList

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -5,9 +5,9 @@ import { Observable, of } from 'rxjs';
 @Injectable()
 export class NoteService {
     private notes: Array<Note> = [
-        new Note(1, "Заметка 1", Priority.Normal, new Date(2018, 11, 13)),
-        new Note(2, "Заметка 2", Priority.Low, new Date(2018, 11, 14)),
-        new Note(3, "Заметка 3", Priority.High, new Date(2018, 11, 16))
+        new Note(1, "Заметка 1", Priority.Normal, new Date(2018, 10, 13)),
+        new Note(2, "Заметка 2", Priority.Low, new Date(2018, 10, 14)),
+        new Note(3, "Заметка 3", Priority.High, new Date(2018, 10, 16))
     ];
 
     constructor() {}
